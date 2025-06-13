aeca59091d0e4342fe9aa815d6acae2d27b519bb
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-06-14 02:49:06 +0530

    feat: add cors middleware

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.db import create_db_and_tables
 from contextlib import asynccontextmanager
 from src.routes.snippets import router as snippets_router
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
 
 
 @asynccontextmanager
@@ -13,6 +18,16 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+CORS_ORIGINS = os.getenv("CORS_ORIGIN", "http://localhost:3000")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ORIGINS,  # Uses origins from environment variable
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(snippets_router, prefix="/snippets", tags=["snippets"])
 
